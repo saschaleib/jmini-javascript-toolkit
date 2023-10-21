769a861fb7bfb85f9559a3cdb2d53e5a3b8168ed
saschaleib <sascha@leib.be>
2023-10-21 14:20:57 +0200

    Reorganized Elements functions
    First step towards a builder script

---
+++ dev/elements/Element.new.js
@@ -0,0 +1,15 @@
+/* creates a new Element */
+/* parameter: name (String) the name of the element, */
+/* parent object: Element */
+/* returns: the value of the attribute (String) if no value is set, */
+/* returns: the parent element (Object) if value is set */
+/* Support: DOM Level 1 (1998) */
+Element.new = function(n) {
+	var r = null;
+	try {
+		r = document.createElement(n);
+	} catch(e) {
+		console.error(e);
+	}
+	return r;
+}
---
+++ dev/elements/Element.text.js
@@ -0,0 +1,13 @@
+/* Sets or gets the text content of an element */
+/* parameter: (optional) text to set */
+/* parent object: Element */
+/* returns: the (modified) parent object */
+/* Support: DOM Level 1 (1998) */
+Element.prototype.text = function(txt = null) {
+	if (txt) {
+		this.textContent = txt;
+		return this;
+	} else {
+		return this.textContent;
+	}
+}
