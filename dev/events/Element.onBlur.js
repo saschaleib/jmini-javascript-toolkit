43795efb6851ca9b51aeb2a88c4bfad7e8651bbf
saschaleib <sascha@leib.be>
2023-10-23 23:09:47 +0200

    Element.onFocus and onBlur functions

---
+++ dev/events/Element.onBlur.js
@@ -0,0 +1,8 @@
+/* Attaches a listener for the "blur" event to an Element */
+/* parameter: (Function, required) the callback function */
+/* parent object: Element */
+/* returns: Element (passes through the parent element) */
+Element.prototype.onBlur = function(cb) {
+	this.addEventListener('blur', cb);
+	return this;
+}
---
+++ dev/events/Element.onFocus.js
@@ -0,0 +1,8 @@
+/* Attaches a listener for the "focus" event to an Element */
+/* parameter: (Function, required) the callback function */
+/* parent object: Element */
+/* returns: Element (passes through the parent element) */
+Element.prototype.onFocus = function(cb) {
+	this.addEventListener('focus', cb);
+	return this;
+}
