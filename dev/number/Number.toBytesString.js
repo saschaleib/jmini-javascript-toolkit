da459e7b5b8520cc8bbf678e8ce3ded925e793c4
Sascha Leib <saschaleib@users.noreply.github.com>
2023-10-23 11:41:16 +0200

    New function
    New function: Number.toBytesString()

--- dev/number/Number.toBytesString.js
+++ dev/number/Number.toBytesString.js
@@ -5,6 +5,19 @@
 /* parent object: Number */
 /* returns: the (modified) parent object */
 /* Support: DOM Level 1 (1998) */
-Number.prototype.toBytesString = function(digits = 2, locale = 'en', localOptions = null) {
-	// TODO!
+Number.prototype.toBytesString = function(digits = 2, loc = 'en-US') {
+
+	units = ['Bytes','KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB','???'];
+	n2 = Math.floor(this);
+
+	p = 0;
+	while (n2 > 980 && p < units.length) {
+		n2 = n2/1024;
+		p += 1;
+	}
+	let f = new Intl.NumberFormat(loc, {
+			maximumSignificantDigits: digits
+			});
+
+	return f.format(n2) + '\u202F' + units[p];
 }
