1c3e86629a589d47dbf98462493cc12554f52210
saschaleib <sascha@leib.be>
2023-10-23 23:53:54 +0200

    onSubmit

--- dev/events/Form.onSubmit.js
+++ dev/events/Form.onSubmit.js
@@ -1,9 +1,8 @@
-/* Attaches a listener for the "focus" event to an Element */
+/* Attaches a listener for the "focus" event to a <form> Element */
 /* parameter: (Function, required) the callback function */
 /* parent object: Element */
 /* returns: Element (passes through the parent element) */
 Element.prototype.onSubmit = function(cb) {
-	console.log(this);
 	if(this.localName !== 'form') {
 		throw new Error("the .onSubmit function can only be applied to <form> elements.");
 	} else {
