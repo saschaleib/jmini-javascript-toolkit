b1feb3088420130f8442b448df4ba4b5318448bb
Sascha Leib <saschaleib@users.noreply.github.com>
2023-10-26 15:39:07 +0200

    Documentation
    Starting to wrote documentation pages

---
+++ docs/_theme/docs.js
@@ -0,0 +1,22 @@
+'use strict'; // Encoding: UTF-8
+/**
+ * jMini Documentation pages scripts
+ *
+ * @link     https://github.com/saschaleib/jMini
+ * @author   Sascha Leib <ad@hominem.info>
+ * @license  GPL 3 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @version 0.0.1
+ * @date 2023-10-26
+ */
+
+let $P = {
+	
+	/* global init */
+	init: function() {
+		console.info('$P.init()');
+	}
+}
+
+/* init when document is ready: */
+this.addEventListener('DOMContentLoaded', $P.init, false);
